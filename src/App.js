477519cc19fbd4266b1821639114aced61ba91cb
saidileeppavuluru <saidileeppavuluru@gmail.com>
2026-01-07 14:42:39 +0530

    Dileep Updated

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import MyFunction from './Components/MyFunction';
-import Home from './Components/Home';
-import AboutUs from './Components/AboutUs';
-import Blog from './Components/Blog';
-import Contact from './Components/Contact';
-import ClassComponentEx from './Components/ClassComponentEx';
-import FunctionComponentEx from './Components/FunctionComponentEx';
-import PrviosCount from './Components/PrviosCount';
-import EventHandlers from './Components/EventHandlers';
-import Form1 from './Components/Form1';
-import ParentComponent from './Components/ParentComponent';
-import Greet from './Components/Greet';
-import Timer from './Components/Timer';
-import UserData from './Components/UserData';
-import AxiosExample from './Components/AxiosExample';
-
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import MyFunction from "./Components/MyFunction";
+import Home from "./Components/Home";
+import AboutUs from "./Components/AboutUs";
+import Blog from "./Components/Blog";
+import Contact from "./Components/Contact";
+import ClassComponentEx from "./Components/ClassComponentEx";
+import FunctionComponentEx from "./Components/FunctionComponentEx";
+import PrviosCount from "./Components/PrviosCount";
+import EventHandlers from "./Components/EventHandlers";
+import Form1 from "./Components/Form1";
+import ParentComponent from "./Components/ParentComponent";
+import Greet from "./Components/Greet";
+import Timer from "./Components/Timer";
+import UserData from "./Components/UserData";
+import AxiosExample from "./Components/AxiosExample";
+import Dileep from "./Components/Dileep";
 
 function App() {
   return (
@@ -38,29 +38,28 @@ function App() {
     //         <Route path="/Contact" element={<Contact />} />
     //         <Route path="/MyFun" element={<MyFunction />} />
 
-
     //       </Routes>
-             
+
     //     </>
     //   </div>
     // </Router>
     <>
-     {/* <ClassComponentEx/>
+      {/* <ClassComponentEx/>
      <FunctionComponentEx/> */}
-     {/* <PrviosCount/> */}
-     {/* <EventHandlers/> */}
-     {/* <Form1/> */}
-     {/* <Greet name="Jon"/>
+      {/* <PrviosCount/> */}
+      {/* <EventHandlers/> */}
+      {/* <Form1/> */}
+      {/* <Greet name="Jon"/>
      <Greet name="Tom"/>
      <Greet name="Stanly"/> */}
       {/* <ParentComponent/> */}
 
       {/* <FunctionComponentEx/> */}
 
-      
-        {/* <Timer/> */}
-        {/* <UserData userId={2}/> */}
-        <AxiosExample/>
+      {/* <Timer/> */}
+      {/* <UserData userId={2}/> */}
+      <Dileep />
+      <AxiosExample />
     </>
   );
 }
---
+++ src/Components/Dileep.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dileep = () => {
+  return (
+    <div>
+      <h1> Hello I am Dileep From grow Tech </h1>
+    </div>
+  );
+};
+
+export default Dileep;
